2d393e91b6732f1ea0d985206b431ff52b1f5160
maivey <51893020+maivey@users.noreply.github.com>
2020-03-22 18:15:38 -0700

    add python file that has function get_coords

---
+++ coords.py
@@ -0,0 +1,14 @@
+def get_coords(street_num, street_name, city, state):
+    import requests
+    import json
+    from api_keys import gkey
+    
+    base_url = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+    #'{street_num}+{street_name}+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY'
+    url2 = f'{street_num}+{street_name},+{city},+{state}&key={gkey}'
+    url = base_url + url2
+    response = requests.get(url)
+    json_response = response.json()
+    person_lat = json_response['results'][0]['geometry']['location']['lat']
+    person_lon = json_response['results'][0]['geometry']['location']['lng']
+    return person_lat, person_lon
